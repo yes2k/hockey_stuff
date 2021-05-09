361a2f6905d9a9559cd2ff7ee7d278d34aa4f4da
Yatharth Khattar <yatharthkhattar@gmail.com>
2021-05-08 21:50:36 -0400

    shiny app

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1,10 +1,10 @@
 source("in_game_win_prob_tracker_functions.R")
-
+source("xg_functions.R")
 
 ui <- fluidPage(
   
   # App title ----
-  headerPanel("Live Win Probablility Tracker"),
+  headerPanel("Game "),
   
   # Sidebar panel for inputs ---- 
   sidebarPanel(
@@ -12,18 +12,45 @@ ui <- fluidPage(
              it, input the game id you are looking for into the
              textbox and the in game probabilty graph will appear to 
              the right"),
-    numericInput("game_id", "Write game id", 2019020809)
+    dateInput("date", "Select Date"),
+    uiOutput("game_selection"),
+    
   ),
   
   # Main panel for displaying outputs ----
   mainPanel(
-    plotOutput("live_prob_graph")
+    tabsetPanel(type="tabs",
+                tabPanel("Cumulative xG plot", 
+                         plotOutput("xg_plot") %>% withSpinner()),
+                tabPanel("Win Probability Plot", 
+                         plotOutput("prob_graph") %>% withSpinner())
+                )
   )
 )
 
 server <- function(input, output){
-  output$live_prob_graph <- renderPlot({
-    rt_probs_graph_for_id(input$game_id)
+  obsList <- list()
+  
+  # Initial empty plot so that the loading icon doesn't continuously show
+  output$xg_plot <- renderPlot({plot.new()})
+  output$prob_graph <- renderPlot({plot.new()})
+  
+  output[["game_selection"]] <- renderUI({
+    obsList <<- list()
+    d <- input[["date"]]
+    sc.scrape_schedule(start_date = d, 
+                       end_date = d, print_sched = FALSE) %>% 
+      dplyr::select(game_id, home_team, away_team) %>% 
+      as.list(.) %>% pmap(function(game_id, home_team, away_team){
+        btn_id <- paste0("game_selection_button_", game_id)
+        if(is.null(obsList[[btn_id]])){
+          obsList[[btn_id]] <<- observeEvent(input[[btn_id]],{
+            output$xg_plot <- renderPlot({xG_graph(game_id)})
+            output$prob_graph <- renderPlot({rt_probs_graph_for_id(game_id)})
+          })
+          actionButton(btn_id, paste(away_team, "@", home_team))
+        }
+      }) 
   })
 }
 
--- shinyapp/in_game_win_prob_tracker_functions.R
+++ shinyapp/in_game_win_prob_tracker_functions.R
@@ -1,14 +1,18 @@
 library(shiny)
+library(shinycssloaders)
+library(scales)
 library(RCurl); library(xml2); library(rvest); library(jsonlite); library(foreach)
 library(lubridate)
 library(tidyverse)
+library(furrr)
 source("EH_scrape_functions.R")
 
-model <- readRDS("ingame_win_prob_model.RDS")
+win_prob_model <- readRDS("ingame_win_prob_model.RDS")
 
 data_pipeline <- function(pbp){
   cols_to_select <- c("game_id", "game_seconds","home_team", "away_team",
-                      "home_score", "away_score", "game_strength_state", "event_type")
+                      "home_score", "away_score", "game_strength_state", 
+                      "event_type")
   
   events_to_remove <- c("GSTART", "GEND", "PSTART", "PEND")
   
@@ -36,17 +40,29 @@ data_pipeline <- function(pbp){
 
 rt_probs_graph <- function(model, pbp, game_info){
   new_pbp <- data_pipeline(pbp)
-  prob_home_team_winging <- predict(model, newdata = new_pbp %>% dplyr::select(-c("game_id")), type="response")
+  prob_home_team_winging <- predict(model, newdata = new_pbp %>% 
+                                      dplyr::select(-c("game_id")), 
+                                        type="response")
   new_pbp <- cbind(new_pbp, prob_home_team_winging)
-  p <- ggplot(new_pbp, aes(x=game_seconds, y=prob_home_team_winging)) + 
-          geom_line() + ggtitle(paste("In Game Win Probability Tracker ", game_info$away_team, " @ ", 
-                                      game_info$home_team, ", ", game_info$game_date, sep="")) +
-          theme(plot.title = element_text(hjust = 0.5))
+  p <- ggplot(new_pbp, aes(x=game_seconds/60, y=prob_home_team_winging)) + 
+          geom_line() + ggtitle(paste("In Game Win Probability Tracker ", 
+                                      game_info$away_team, " @ ", 
+                                      game_info$home_team, ", ", 
+                                      game_info$game_date, sep="")) +
+          theme(plot.title = element_text(hjust = 0.5)) + 
+          coord_cartesian(ylim=c(0, 1)) + 
+    scale_x_continuous(name="Game Minutes", breaks = seq(0, 65, 10)) + 
+    scale_y_continuous(name = paste("Probability of", 
+                                    game_info$home_team, "winning"), 
+                       labels=percent) + 
+    theme_classic()
+      
   return(p)
 }
 
 rt_probs_graph_for_id <- function(gid){
-  game_data <- sc.scrape_pbp(as.character(gid))
-  probs_graph <- rt_probs_graph(model, game_data$pbp_base, game_data$game_info)
+  game_data <- sc.scrape_pbp(as.character(gid), live_scrape = TRUE)
+  probs_graph <- rt_probs_graph(win_prob_model, 
+                                game_data$pbp_base, game_data$game_info)
   return(probs_graph)
 }
